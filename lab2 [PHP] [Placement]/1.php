dd05947296646d8d0344edb0ced6d169772bca69
Taslima Akter Tuli <77536163+TaslimaTuli@users.noreply.github.com>
2023-02-27 20:40:41 +0600

    Delete lab2 [PHP] [Placement] directory

--- lab2 [PHP] [Placement]/1.php
+++
@@ -1,8 +0,0 @@
-<?php  
-  $length = 44;  
-  $width = 55;
-
-  echo "The area of a Rectangle $length * $width =  " . ($length * $width) . ", "; 
-  echo "perimeter 2 * $length * $width = " . (2*($length+$width)) . "<br />"; 
-	
-?> 
--- lab2 [PHP] [Placement]/2.php
+++
@@ -1,6 +0,0 @@
-<?php  
-  $amount = 1050;  
-  $vat = $amount * 0.15;
-
-  echo "Vat of $amount tk is $vat tk"; 	
-?> 
